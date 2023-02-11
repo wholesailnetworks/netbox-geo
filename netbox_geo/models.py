e354980e99e2017c5762c6b6fbeffca09782968e
Nick Bogle <nick@ionswitch.com>
2023-02-11 14:20:04 -0500

    Add Paths & Polygons

--- netbox_geo/models.py
+++ netbox_geo/models.py
@@ -49,27 +49,94 @@ class Point(NetBoxModel):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('plugins:netbox_geo:geo', args=[self.pk])
+        return reverse('plugins:netbox_geo:points', args=[self.pk])
 
     def clean(self):
         super().clean()
 
-# class Paths(models.Model):
-#     name = models.CharField(max_length=50)
-#     area = models.IntegerField()
-#     pop2005 = models.IntegerField('Population 2005')
-#     fips = models.CharField('FIPS Code', max_length=2, null=True)
-#     iso2 = models.CharField('2 Digit ISO', max_length=2)
-#     iso3 = models.CharField('3 Digit ISO', max_length=3)
-#     un = models.IntegerField('United Nations Code')
-#     region = models.IntegerField('Region Code')
-#     subregion = models.IntegerField('Sub-Region Code')
-#     lon = models.FloatField()
-#     lat = models.FloatField()
+class Paths(NetBoxModel):
+    name = models.CharField(max_length=100)
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+    a_site = models.ForeignKey(
+        to='dcim.Site',
+        on_delete=models.PROTECT,
+        related_name="%(class)s_a_related",
+        blank=True,
+        null=True
+    )
+    z_site = models.ForeignKey(
+        to='dcim.Site',
+        on_delete=models.PROTECT,
+        related_name="%(class)s_z_related",
+        blank=True,
+        null=True
+    )
+    circuit = models.ForeignKey(
+        to='circuits.Circuit',
+        on_delete=models.PROTECT,
+        related_name="%(class)s_related",
+        blank=True,
+        null=True
+    )
+    tenant = models.ForeignKey(
+        to='tenancy.Tenant',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    path = models.LineStringField()
+    class Meta:
+        verbose_name = 'Paths'
+        verbose_name_plural = 'Paths'
+        
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('plugins:netbox_geo:paths', args=[self.pk])
 
-#     # GeoDjango-specific: a geometry field (MultiPolygonField)
-#     mpoly = models.MultiPolygonField()
+    def clean(self):
+        super().clean()
+        
+class Polygons(NetBoxModel):
+    name = models.CharField(max_length=100)
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+    sitegroups = models.ForeignKey(
+        to='dcim.SiteGroup',
+        on_delete=models.PROTECT,
+        related_name="%(class)s_related",
+        blank=True,
+        null=True
+    )
+    regions = models.ForeignKey(
+        to='dcim.Region',
+        on_delete=models.PROTECT,
+        related_name="%(class)s_related",
+        blank=True,
+        null=True
+    )
+    tenant = models.ForeignKey(
+        to='tenancy.Tenant',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    polygon = models.PolygonField()
+    class Meta:
+        verbose_name = 'Polygons'
+        verbose_name_plural = 'Polygons'
+        
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('plugins:netbox_geo:polygons', args=[self.pk])
 
-#     # Returns the string representation of the model.
-#     def __str__(self):
-#         return self.name
+    def clean(self):
+        super().clean()
